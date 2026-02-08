1577dffbeb618b76c58d3b70fa670b1d6f828f66
Sadiksha KC Neupane <kcneupanesadiksha@gmail.com>
2026-02-08 14:43:54 +0545

    modified:   mountains.json         modified:   script.js         modified:   style.css

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+let mountains = [];
 
+fetch("mountains.json")
+  .then((response) => response.json())
+  .then((data) => {
+    mountains = data;
+  });
+
+function searchMountain() {
+  const input = document.getElementById("searchInput").value.toLowerCase();
+  const resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = "";
+
+  const found = mountains.find((mountain) =>
+    mountain.name.toLowerCase().includes(input),
+  );
+
+  if (found) {
+    resultDiv.innerHTML = `<h2> ${found.name} </h2>
+    <p>${found.country}</p>
+    <p>${found.height}</p>
+    <p>${found.range}</p>
+    <p>${found.description}</p>
+    <p>${found.image}</p>
+    <img src="${found.image}" alt="${found.name}" style="max-width:300px;"/>
+    `;
+  } else {
+    resultDiv.innerHTML = "<p>NO MOUNTAINS FOUND </p>";
+  }
+}
+
+document
+  .getElementById("searchButton")
+  .addEventListener("click", searchMountain);
