4430a398dd5b671e4cb97bd849b1ac3adf363fa2
Sadiksha KC Neupane <kcneupanesadiksha@gmail.com>
2026-02-09 13:26:41 +0545

    fixed sytling image sizes

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ function searchMountain() {
   );
 
   if (found) {
-    resultDiv.innerHTML = `<h2> ${found.name} </h2>
-    <p class="mountain-info">${found.country}</p>
+    resultDiv.innerHTML = `<h2><strong>NAME:</strong> ${found.name} </h2>
+    <p class="mountain-info"> ${found.country}</p>
     <p class="mountain-info" >${found.height}</p>
     <p class="mountain-info">${found.range}</p>
     <p class="mountain-info">${found.description}</p>
-    <img src="${found.image}" alt="${found.name}" style="max-width:300px;"/>
+    <img src="${found.image}" alt="${found.name}" style="max-width:500px;"/>
 
     `;
   } else {
